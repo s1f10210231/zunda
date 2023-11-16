5007c429ab2fc9f0a532c5de317e6b7614d2256a
s1f10210231 <s1f102102313@iniad.org>
2023-11-16 18:29:33 +0900

    static_root設定

--- config/settings.py
+++ config/settings.py
@@ -126,4 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'story_app/static/story_app'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'proz/static/proz'),)
