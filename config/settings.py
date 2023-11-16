175bb76554d1b63714efc4fc474076e2e258a8ab
s1f10210231 <s1f102102313@iniad.org>
2023-11-16 18:50:28 +0900

    /

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'proz/static/proz'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'proz/static/proz'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'story_app/staticfiles')   
 
 
