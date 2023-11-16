9f8f5e2710d9035488bc057b054871291bee0c37
s1f10210231 <s1f102102313@iniad.org>
2023-11-17 00:46:08 +0900

    build

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 # exit on error
 set -o errexit
 
+wget https://github.com/VOICEVOX/voicevox_core/releases/download/0.14.3/voicevox_core-0.14.3+cpu-cp38-abi3-linux_x86_64.whl
+wget https://github.com/microsoft/onnxruntime/releases/download/v1.13.1/onnxruntime-linux-x64-1.13.1.tgz
+tar xvzf onnxruntime-linux-x64-1.13.1.tgz
+mv onnxruntime-linux-x64-1.13.1/lib/libonnxruntime.so.1.13.1 ./
+wget http://downloads.sourceforge.net/open-jtalk/open_jtalk_dic_utf_8-1.11.tar.gz
+tar xvzf open_jtalk_dic_utf_8-1.11.tar.gz
+
 pip3 install -r requestment.txt
 
 python3 manage.py collectstatic --no-input
